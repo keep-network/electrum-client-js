ad73c42b85211273b43fc5a5e1462042f19e5b29
Jakub Nowakowski <jakub.nowakowski@akena.co>
2023-01-12 13:11:53 +0100

    Update electrum servers for tests
    We use public Keep Network's server for integration tests.

--- test/config.js
+++ test/config.js
@@ -1,27 +1,29 @@
-const servers = {
-  tcp: {
-    protocol: 'tcp', port: '50001', host: 'electrum.bitaroo.net',
+const servers = [
+  {
+    protocol: 'tcp',
+    port: '80',
+    host: 'electrumx-server.test.tbtc.network',
   },
-  ssl: {
-    protocol: 'ssl', port: '50002', host: 'electrum.bitaroo.net',
+  {
+    protocol: 'ws',
+    port: '8080',
+    host: 'electrumx-server.test.tbtc.network',
   },
-  ws: {
-    protocol: 'ws', port: '50003', host: 'electrumx-server.tbtc.svc.cluster.local',
-  },
-  wss: {
-    protocol: 'wss', port: '50004', host: 'electrumx-server.tbtc.svc.cluster.local',
-  },
-}
 
-const serversArray = [
-  servers.tcp,
-  servers.ssl,
-  // FIXME: WebSocket is commented out for CI, until we find public servers for this protocol.
-  // electrumServers.ws,
-  // electrumServers.wss,
+  // FIXME: The client doesn't work with SSL connection.
+  // ssl: {
+  //   protocol: "ssl",
+  //   port: "443",
+  //   host: "electrumx-server.test.tbtc.network",
+  // },
+  // FIXME: The client doesn't work with WSS connection.
+  // wss: {
+  //   protocol: "wss",
+  //   port: "8443",
+  // host: "electrumx-server.test.tbtc.network",
+  // },
 ]
 
 module.exports = {
   servers,
-  serversArray,
 }
