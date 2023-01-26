c3de62fd0f42b84ea10e8cd0fa147ff1c5015e4a
Jakub Nowakowski <jakub.nowakowski@akena.co>
2023-01-26 13:15:40 +0100

    Add more test servers
    Here we add more tests servers for integration tests.

--- test/config.js
+++ test/config.js
@@ -1,26 +1,65 @@
-const ELECTRUMX_HOST = 'electrumx-server.test.tbtc.network'
+const TBTC_ELECTRUMX_HOST = 'electrumx-server.test.tbtc.network'
+const BLOCKSTREAM_ELECTRS_HOST = 'electrum.blockstream.info'
 
 const servers = [
-  {
-    protocol: 'tcp',
-    port: '80',
-    host: ELECTRUMX_HOST,
-  },
-  {
-    protocol: 'ws',
-    port: '8080',
-    host: ELECTRUMX_HOST,
-  },
-  {
-    protocol: 'ssl',
-    port: '443',
-    host: ELECTRUMX_HOST,
-  },
-  // FIXME: The client doesn't work with WSS connection.
+  // TODO: Enable all protocols test for test.tbtc.network servers once they are
+  // publicly exposed.
+  // {
+  //   protocol: 'tcp',
+  //   port: '80',
+  //   host: TBTC_ELECTRUMX_HOST,
+  // },
+  // {
+  //   protocol: 'ssl',
+  //   port: '443',
+  //   host: TBTC_ELECTRUMX_HOST,
+  // },
+  // {
+  //   protocol: 'ws',
+  //   port: '8080',
+  //   host: TBTC_ELECTRUMX_HOST,
+  // },
+
   {
     protocol: 'wss',
     port: '8443',
-    host: ELECTRUMX_HOST,
+    host: TBTC_ELECTRUMX_HOST,
+  },
+  // electrumx tcp
+  {
+    host: 'electrum1.cipig.net',
+    port: '10068',
+    protocol: 'tcp',
+  },
+  // electrumx ssl
+  {
+    host: 'testnet.qtornado.com',
+    port: '51002',
+    protocol: 'ssl',
+  },
+  // electrs-esplora tcp
+  {
+    protocol: 'tcp',
+    port: '60001',
+    host: BLOCKSTREAM_ELECTRS_HOST,
+  },
+  // electrs-esplora ssl
+  {
+    host: 'electrum.blockstream.info',
+    port: '60002',
+    protocol: 'ssl',
+  },
+  // fulcrum tcp
+  {
+    host: 'testnet.aranguren.org',
+    port: 51001,
+    protocol: 'tcp',
+  },
+  // fulcrum ssl
+  {
+    host: 'testnet.aranguren.org',
+    port: 51002,
+    protocol: 'ssl',
   },
 ]
 
